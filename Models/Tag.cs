8d3ba56926b065540eedccbfe47ba9819b96b140
Lain <134944634+Lain4504@users.noreply.github.com>
2025-07-01 07:30:22 +0700

    create project

---
+++ Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HuynhNgocTien_SE18B01_A02.Models;
+
+public partial class Tag
+{
+    public int TagId { get; set; }
+
+    public string? TagName { get; set; }
+
+    public string? Note { get; set; }
+
+    public virtual ICollection<NewsArticle> NewsArticles { get; set; } = new List<NewsArticle>();
+}
